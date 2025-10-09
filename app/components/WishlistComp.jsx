ff26b6bbd82e4c5e538ba6ae0de5a24cf0e19ab7
Shoaib-777 <shaikmohammedshoaib777@gmail.com>
2025-10-09 12:14:25 +0530

    updated wishlist page

--- app/components/WishlistComp.jsx
+++ app/components/WishlistComp.jsx
@@ -7,6 +7,7 @@ import { useCartStore } from '@/store/useCartStore';
 import Link from 'next/link';
 import { getUserIDCSR } from '@/utils/GetCSrUserId';
 import AddToCartWrapper from './AddToCartWrapper';
+import { FaHeart } from 'react-icons/fa';
 
 const WishlistComp = ({session}) => {
     const userId = getUserIDCSR()
@@ -24,6 +25,27 @@ const WishlistComp = ({session}) => {
     useEffect(() => {
         getWishList(userId)
     }, [userId])
+
+    if(!userId){
+        return (
+      <div className="flex items-center justify-center h-[calc(100vh-120px)] bg-gray-50 px-4">
+      <div className="max-w-md w-full bg-white shadow-xl rounded-xl p-8 text-center border border-gray-300">
+        <FaHeart className="mx-auto h-14 w-14 text-pink-500" />
+        <h2 className="mt-6 text-2xl font-semibold text-gray-800">
+          Your Wishlist is empty
+        </h2>
+        <p className="mt-2 text-gray-600">
+          You need to login to save your favorite products.
+        </p>
+        <Link href="/login">
+          <span className="mt-6 inline-block w-full bg-pink-500 hover:bg-pink-600 text-white font-medium py-3 px-6 rounded-lg transition-all duration-300">
+            Go to Login
+          </span>
+        </Link>
+      </div>
+    </div>
+    )
+    }
 
     return (
         <div className=" bg-gray-50 relative">
--- app/wishlist/page.js
+++ app/wishlist/page.js
@@ -1,33 +1,16 @@
-import Link from 'next/link'
-import { FaHeart } from 'react-icons/fa'
+"use client";
 
-import { getServerSession } from "next-auth";
-import { authOptions } from "../api/auth/[...nextauth]/route";
-import WishlistComp from "../components/WishlistComp";
 
-export default async function Wishlist() {
-  const session = await getServerSession(authOptions);
+import { SessionProvider } from 'next-auth/react';
+import React from 'react'
+import WishlistComp from '../components/WishlistComp';
 
-  if (!session) {
-    return (
-      <div className="flex items-center justify-center h-[calc(100vh-120px)] bg-gray-50 px-4">
-      <div className="max-w-md w-full bg-white shadow-xl rounded-xl p-8 text-center border border-gray-300">
-        <FaHeart className="mx-auto h-14 w-14 text-pink-500" />
-        <h2 className="mt-6 text-2xl font-semibold text-gray-800">
-          Your Wishlist is empty
-        </h2>
-        <p className="mt-2 text-gray-600">
-          You need to login to save your favorite products.
-        </p>
-        <Link href="/login">
-          <span className="mt-6 inline-block w-full bg-pink-500 hover:bg-pink-600 text-white font-medium py-3 px-6 rounded-lg transition-all duration-300">
-            Go to Login
-          </span>
-        </Link>
-      </div>
-    </div>
-    )
+const Wishilst = ({session}) => {
+  return (
+    <SessionProvider session={session}>
+      <WishlistComp session={session}/>
+    </SessionProvider>
+  )
 }
 
-  return <WishlistComp session={session} />;
-}
+export default Wishilst
