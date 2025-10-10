715355f7a202488420bc0448cab3b0db2dab8a80
Shoaib-777 <shaikmohammedshoaib777@gmail.com>
2025-10-10 17:50:52 +0530

    add animations

--- app/components/AddToCartBtn.jsx
+++ app/components/AddToCartBtn.jsx
@@ -3,7 +3,7 @@ import { useCartStore } from '@/store/useCartStore'
 import { useWishlistStore } from '@/store/useWishlistStore'
 import { redirectFromSSR } from '@/utils/ServerActions'
 import { useSession } from 'next-auth/react'
-import React from 'react'
+import React, { useState } from 'react'
 import { BsCart3 } from 'react-icons/bs'
 import { IoHeartCircleOutline } from 'react-icons/io5'
 
@@ -16,6 +16,8 @@ const AddToCartBtn = ({ productId }) => {
     let see2 = wishlistData?.map((item) => item._id.toString())
     const isInCart = see.includes(productId)
     const isInWishList = see2.includes(productId)
+    const [loadingProductId, setLoadingProductId] = useState(null);
+    const [loadingWishlistId, setLoadingWishlistId] = useState(null)
 
 
     const handleAddToCart = async (productId) => {
@@ -26,12 +28,13 @@ const AddToCartBtn = ({ productId }) => {
             }, 3000)
             return;
         }
-
         try {
-            // console.log("iam userid", userId)
+            setLoadingProductId(productId)
             await addToCart(userId, "single_add", productId);
         } catch (err) {
             alert("Something Went Wrong")
+        } finally {
+            setLoadingProductId(null)
         }
     };
 
@@ -44,19 +47,16 @@ const AddToCartBtn = ({ productId }) => {
             return;
         }
         try {
-
+            setLoadingWishlistId(productId)
             if (isInWishList) {
-                // console.log("iam userid", userId)
-
                 await removeFromWishlist(userId, productId);
             } else {
-                // console.log("iam userid", userId)
-
                 await addToWishList(userId, productId);
             }
-
         } catch (err) {
             alert("Something Went Wrong")
+        } finally {
+            setLoadingWishlistId(null)
         }
     };
 
@@ -64,29 +64,44 @@ const AddToCartBtn = ({ productId }) => {
         <>
             <div className="flex justify-between px-1 items-center w-full">
                 {isInCart ? (
-                    <button className="bg-indigo-500 text-white px-4 py-2 mt-1 text-nowrap rounded-lg flex gap-2 justify-center items-center text-center flex-1 hover:bg-indigo-700 transition-colors"
+                    <button
+                        className="btn bg-indigo-500 text-white px-4 py-2 mt-1 text-nowrap rounded-lg flex gap-2 justify-center items-center flex-1 hover:bg-indigo-700 transition-colors"
                         onClick={setShowCartTrue}
                     >
                         <BsCart3 /> View Cart
                     </button>
+                ) : loadingProductId === productId ? (
+                    <button
+                        className="btn bg-black relative text-white px-4 py-2 mt-1 text-nowrap rounded-lg flex gap-2 justify-center items-center flex-1 overflow-hidden"
+                        disabled
+                    >
+                        <span className="relative z-10 flex items-center gap-2">
+                            <BsCart3 /> Adding<span className="loading loading-dots loading-md"></span>
+                        </span>
+                        <span className="absolute inset-0 bg-indigo-500 animate-fillUp z-0"></span>
+                    </button>
                 ) : (
                     <button
-                        className="bg-black text-white px-4 py-2 mt-1 text-nowrap rounded-lg flex gap-2 justify-center items-center text-center flex-1 hover:bg-blue-700 transition-colors"
+                        className="btn bg-black text-white px-4 py-2 mt-1 text-nowrap rounded-lg flex gap-2 justify-center items-center flex-1 hover:bg-blue-700 transition-colors"
                         onClick={() => handleAddToCart(productId)}
                     >
                         <BsCart3 /> Add To Cart
                     </button>
                 )}
-
-
                 <button
-                    className="ml-2 p-2 rounded-full hover:bg-gray-200 transition-colors"
+                    className="ml-2 p-2 rounded-full hover:bg-gray-200 transition-colors overflow-hidden flex items-center justify-center"
                     onClick={() => handleAddToWishlist(productId)}
+                    disabled={loadingWishlistId === productId}
                 >
-                    <IoHeartCircleOutline
-                        className={`w-10 h-10 ${isInWishList ? "fill-pink-600" : "fill-black"}`}
-                    />
+                    {loadingWishlistId === productId ? (
+                        <span className=" loading loading-ring text-pink-500 size-10"></span>
+                    ) : (
+                        <IoHeartCircleOutline
+                            className={`w-10 h-10  ${isInWishList ? "fill-pink-600" : "fill-black"}`}
+                        />
+                    )}
                 </button>
+
             </div>
         </>)
 }
